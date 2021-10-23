75de4d9ca1db35166d11b749f292428e340c1e41
threemay <254363807@qq.com>
2021-10-24 00:03:23 +1100

    1

--- add.sh
+++ add.sh
@@ -1,6 +1,4 @@
-
 #!/bin/bash
-
 
 # the first parameter should be the master branch name
 # the first parameter should be the feature branch name
@@ -9,21 +7,6 @@
 # in feature branch 
 # if no contain, exit with 1 code
 
-
-git checkout $1
-# OUT=$(git rev-parse $1)
-# git checkout $2
-
-# git log | grep -q ${OUT}
-
-git log $1
-
-# if [[ $(git log $2) =~ "${OUT}" ]]
-# then
-#     echo "OK"
-#     exit 0
-# else
-#     echo "no"
-#     exit 1
-# fi
-
+git add .
+git commit -m "1"
+git push origin test
