d7883878b626487d0cb0114225cd23b7fcff3e20
threemay <254363807@qq.com>
2021-10-24 01:02:18 +1100

    1

--- 3GitCheck.sh
+++ 3GitCheck.sh
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 
 #!/bin/bash
 
-=======
-#!/usr/bin/env sh
->>>>>>> 1
-=======
-#!/bin/bash
->>>>>>> 1
+
 
 # the first parameter should be the master branch name
 # the first parameter should be the feature branch name
@@ -17,9 +11,6 @@
 # in feature branch 
 # if no contain, exit with 1 code
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 git fetch origin $1 
 
 
@@ -34,51 +25,12 @@ OUT=$(git rev-parse $1)
 # git log $1
 
 if [[ $(git log) =~ "${OUT}" ]]
-then/ma
-=======
-
-git checkout $1
-=======
-git branch
-<<<<<<< HEAD
-# git checkout $1
->>>>>>> 1
-=======
-git checkout main
->>>>>>> 1
-=======
-git fetch origin main
-<<<<<<< HEAD
-git log main
->>>>>>> 1
-=======
-
-git branch
->>>>>>> 1
-# OUT=$(git rev-parse $1)
-# git checkout $2
-# git log | grep -q ${OUT}
-
-# git log $1
-
-<<<<<<< HEAD
-if [[ $(git log $2) =~ "${OUT}" ]]
 then
->>>>>>> 1
     echo "OK"
     exit 0
 else
     echo "no"
     exit 1
 fi
-=======
-# if [[ $(git log $2) =~ "${OUT}" ]]
-# then/ma
-#     echo "OK"
-#     exit 0
-# else
-#     echo "no"
-#     exit 1
-# fi
->>>>>>> 1
+
 
